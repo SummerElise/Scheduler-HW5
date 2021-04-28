2806c2bfe7cef00c39d6e4051af03d896d5af0de
lissy <slissy513@gmail.com>
2021-04-28 18:19:03 -0400

    fixed by timeblock rows and added color for past, present, future times; working on getting my save button to work

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 var today = moment();
 $("#currentDay").text(today.format("LLLL"));
 
-var rows = document.getElementsByClassName("row");
-var currentHour = parseInt(moment().format('H'));
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+Array.from(rows).forEach(row => {
+let 
+rowIdString = row.id,
+rowHour;
+if (rowIdString) {
+    rowHour = parseInt(rowIdString);
+}
+if (rowHour) {
+ if (currentHour === rowHour) {
+     setColor(row, "red");
+ }   else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+     setColor(row,"green");
+ }   else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+     setColor(row, "lightgrey");
+ }   else {
+     setColor(row, "white");
+ }
+}
+});
+
+function setColor(element, color) {
+    element.style.backgroundColor = color;
+}
